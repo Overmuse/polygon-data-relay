93e0972eded1d3df435e4fc5584400e439094a5c
Sebastian Rollen <seb@overmu.se>
2021-02-22 22:06:43 -0500

    working docker image

--- Dockerfile
+++ Dockerfile
@@ -27,5 +27,6 @@ RUN --mount=type=ssh cargo build --release --bin polygon-data-relay
 FROM debian:buster-slim as runtime
 WORKDIR polygon-data-relay
 COPY --from=builder /polygon-data-relay/target/release/polygon-data-relay /usr/local/bin
-ENV RUST_LOG=polygon-data-relay=debug
+ENV RUST_LOG=polygon_data_relay=debug
+RUN apt-get update && apt-get -y install ca-certificates libssl-dev && rm -rf /var/lib/apt/lists/*
 ENTRYPOINT ["/usr/local/bin/polygon-data-relay"]
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use anyhow::{Context, Result};
 use dotenv::dotenv;
 use polygon::ws::Connection;
-use polygon_data_relay::run;
+use polygon_data_relay::{
+    run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 use std::env;
+use tracing::{debug, info};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _guard = sentry::init(sentry::ClientOptions::new());
-    dotenv()?;
-    env_logger::builder().format_timestamp_micros().init();
+    let _ = dotenv();
+    let subscriber = get_subscriber("trader".into(), "info".into());
+    init_subscriber(subscriber);
+    info!("Starting polygon-data-relay");
 
-    let tickers: Vec<String> = env::var("TICKERS")?
+    let tickers: Vec<String> = env::var("TICKERS")
+        .context("Could not find TICKERS")?
         .split(',')
         .map(|x| x.to_string())
         .collect();
@@ -18,23 +24,25 @@ async fn main() -> Result<()> {
 
     if env::var("QUOTES").is_ok() {
         data.push("Q".to_string());
+        debug!("Will subscribe to Quotes");
     }
     if env::var("TRADES").is_ok() {
         data.push("T".to_string());
+        debug!("Will subscribe to Trades");
     }
     if env::var("SECOND_AGGREGATES").is_ok() {
         data.push("A".to_string());
+        debug!("Will subscribe to SecondAggregates");
     }
     if env::var("MINUTE_AGGREGATES").is_ok() {
         data.push("AM".to_string());
+        debug!("Will subscribe to MinuteAggregates");
     }
-    let ws = Connection::new(
-        env::var("POLYGON_KEY").context("Could not find POLYGON_KEY")?,
-        data,
-        tickers,
-    )
-    .connect()
-    .await?;
+    let token = env::var("POLYGON_KEY").context("Could not find POLYGON_KEY")?;
+    let ws = Connection::new(token, data, tickers)
+        .connect()
+        .await
+        .context("Failed to conect to the WebSocket")?;
 
     run(ws).await;
     Ok(())
