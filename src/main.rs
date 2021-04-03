b3f3fc3f1d87c54f488422937a3570e918992bcc
Sebastian Rollen <seb@overmu.se>
2021-04-03 17:51:37 -0400

    fix config and deployment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use dotenv::dotenv;
 use polygon::ws::Connection;
 use polygon_data_relay::relay::run;
 use polygon_data_relay::server::launch_server;
 use polygon_data_relay::settings::Settings;
-use std::env;
 use std::sync::mpsc::channel;
 use std::thread;
 use tracing::{debug, info, subscriber::set_global_default};
@@ -21,36 +20,43 @@ fn main() -> Result<()> {
     let settings = Settings::new()?;
     info!("Starting polygon-data-relay");
 
-    let tickers: String = env::var("TICKERS").context("Could not find TICKERS")?;
     let mut data: Vec<&str> = vec![];
 
-    if env::var("QUOTES").is_ok() {
+    if settings.polygon.quotes {
         data.push("Q");
         debug!("Will subscribe to Quotes");
     }
-    if env::var("TRADES").is_ok() {
+    if settings.polygon.trades {
         data.push("T");
         debug!("Will subscribe to Trades");
     }
-    if env::var("SECOND_AGGREGATES").is_ok() {
+    if settings.polygon.second_aggregates {
         data.push("A");
         debug!("Will subscribe to SecondAggregates");
     }
-    if env::var("MINUTE_AGGREGATES").is_ok() {
+    if settings.polygon.minute_aggregates {
         data.push("AM");
         debug!("Will subscribe to MinuteAggregates");
     }
-    let base_url = env::var("POLYGON_BASE_URL").context("Cound not find POLYGON_BASE_URL")?;
-    let token = env::var("POLYGON_KEY").context("Could not find POLYGON_KEY")?;
 
     let (tx, rx) = channel();
 
     let kafka_settings = settings.kafka;
     let server_settings = settings.server;
+    let polygon_settings = settings.polygon;
 
     thread::spawn(move || {
-        let tickers: Vec<&str> = tickers.split(',').collect();
-        let connection = Connection::new(&base_url, &token, &data, &tickers);
+        let half_owned: Vec<_> = polygon_settings
+            .tickers
+            .iter()
+            .map(|x| x.as_ref())
+            .collect();
+        let connection = Connection::new(
+            &polygon_settings.base_url,
+            &polygon_settings.key_id,
+            &data,
+            &half_owned,
+        );
         let tokio_runtime = tokio::runtime::Runtime::new().unwrap();
         tokio_runtime.block_on(async {
             run(&kafka_settings, connection, rx).await.unwrap();
--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,6 @@
-use config::{Config, ConfigError, Environment, File};
+use config::{Config, ConfigError, Environment};
 use kafka_settings::KafkaSettings;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 
 #[derive(Debug, Deserialize)]
 pub struct WebServerSettings {
@@ -9,16 +9,40 @@ pub struct WebServerSettings {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct PolygonSettings {
+    pub base_url: String,
+    pub key_id: String,
+    #[serde(deserialize_with = "vec_from_str", default)]
+    pub tickers: Vec<String>,
+    #[serde(default)]
+    pub trades: bool,
+    #[serde(default)]
+    pub quotes: bool,
+    #[serde(default)]
+    pub minute_aggregates: bool,
+    #[serde(default)]
+    pub second_aggregates: bool,
+}
+
+pub fn vec_from_str<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s.split(',').map(From::from).collect())
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Settings {
     pub server: WebServerSettings,
     pub kafka: KafkaSettings,
+    pub polygon: PolygonSettings,
 }
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
         let mut s = Config::new();
-        s.merge(File::with_name("config/local").required(false))?;
-        s.merge(Environment::with_prefix("app"))?;
+        s.merge(Environment::new().separator("__"))?;
         s.try_into()
     }
 }
